b5912d1f9508e47289883d277d6de78949f5db99
Srinivasa kalyan <mskalyan2004@gmail.com>
2026-04-02 09:26:35 +0530

    import dj_databse_url

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-import dj_databse_url
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file  
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
