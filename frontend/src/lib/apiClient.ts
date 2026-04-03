2b6e5d9825af33baa69477ce7c4c03117bb6a027
Srinivasa kalyan <mskalyan2004@gmail.com>
2026-04-03 15:51:09 +0530

    fix baseurl

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const API=process.env.NEXT_PUBLIC_API_URL ;
+
 export const apiClient = axios.create({
-  baseURL: `${API}/api`,
+  baseURL: "https://ai-prep-3cnt.onrender.com/api",
   withCredentials: true,
 });
-
 apiClient.interceptors.response.use(
   (res) => res,
   async (error) => {
